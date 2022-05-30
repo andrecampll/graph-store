d94e5a0300255170c04650ebc948c09136e8e9ee
andrecampll <andrevictor50@gmail.com>
2022-05-30 09:24:21 -0300

    feat: adding Modal component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import { ModalProvider } from '../hooks/useModal';
 import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ModalProvider>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ModalProvider>
   );
 }
 
