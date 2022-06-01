14a92d4ff8cee14070506949ae941ef22cdab4cf
andrecampll <andrevictor50@gmail.com>
2022-06-01 18:40:58 -0300

    feat: integrating Highlight and adding recommended products

--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -1,9 +1,29 @@
 import { Flex as Wrapper, Heading, useMediaQuery } from '@chakra-ui/react';
+import { Product } from '../graphql/generated/graphql';
 import { About } from './About';
 import { Banner } from './Banner';
 import { Button } from './Button';
 
-export const Highlight = () => {
+type Props = Pick<Product, 'name' | 'featured' | 'description'> & {
+  recommendedProducts: {
+    slug: string;
+    imageUrl: string;
+  }[];
+  image?: {
+    url: string;
+    height?: number;
+    size?: number;
+    width?: number;
+  };
+};
+
+export const Highlight = ({
+  name,
+  image,
+  featured,
+  description,
+  recommendedProducts,
+}: Props) => {
   const [isLargerThan768] = useMediaQuery('(min-width: 768px)');
 
   return (
@@ -20,22 +40,23 @@ export const Highlight = () => {
         justifyContent="space-between"
         alignItems="center"
       >
-        <Heading>Samurai King Resting</Heading>
+        <Heading>{name}</Heading>
 
         {isLargerThan768 && <Button w="257px">add to cart</Button>}
       </Wrapper>
 
-      <Banner
-        imageUrl="https://images.pexels.com/photos/2187304/pexels-photo-2187304.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        highlight
-      />
+      <Banner imageUrl={image?.url} highlight={featured} />
 
       {!isLargerThan768 && <Button mb="8">add to cart</Button>}
 
       <About
-        name="Samurai King Resting"
-        aboutText="So how did the classical Latin become so incoherent? According to McClintock, a 15th century typesetter likely scrambled part of Ciceros De Finibus in order to provide placeholder text to mockup various fonts for a type specimen book."
+        recommendedProducts={recommendedProducts}
+        name={name}
+        aboutText={description}
         category={isLargerThan768 ? 'Pets' : ''}
+        dimensions={{
+          ...image,
+        }}
       />
     </Wrapper>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,45 @@
 import { Box } from '@chakra-ui/react';
+import { useMemo } from 'react';
 import { FilterModal } from '../components/Modal/FilterModal';
 import { Header } from '../components/Header';
 import { Highlight } from '../components/Highlight';
 import { ProductsList } from '../components/ProductsList';
 import { useModal } from '../hooks/useModal';
+import { useGetProductsQuery } from '../graphql/generated/graphql';
 
 export default function Home() {
   const { isOpen } = useModal();
 
+  const { data } = useGetProductsQuery({
+    variables: {
+      where: {
+        OR: [
+          {
+            featured: true,
+          },
+        ],
+      },
+    },
+  });
+
+  const recommendedProducts = useMemo(
+    () =>
+      data?.products.flatMap(product =>
+        product.recommendations.map(recommendation => ({
+          slug: recommendation.slug,
+          imageUrl: recommendation.image.url,
+        })),
+      ),
+    [data?.products],
+  );
+
   return (
     <Box margin="0 auto" maxW="1290">
       <Header />
-      <Highlight />
+      <Highlight
+        {...data?.products[0]}
+        recommendedProducts={recommendedProducts}
+      />
 
       <ProductsList />
 
